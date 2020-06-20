d8d0d94fdea24b5e5916e075467e8d329c8a781e
aksis <ivars7@icloud.com>
2020-06-20 18:44:52 +0300

    Update demo.sh

--- bin/demo.sh
+++ bin/demo.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
+
+set -e
+set -u
+set -x
+
 echo "Start demo script"
-env
-echo "$0"
-echo "$1"
-echo "$2"
-echo "$3"
+uname -s
+
+if [ "$(uname -s)" != "Linux" ]; then
+	echo "OS not supported"
+	exit 1
+fi
+
+if [ -f /etc/os-release ]; then
+	. /etc/os-release
+else
+	echo "OS info not found"
+	exit 1
+fi
+
+if [ "$ID" != "raspbian" ]; then
+	echo "Distro not supported"
+	exit 1
+fi
+
+echo 
